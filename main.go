00ba4d6badba664df11129f4dc709fcc67ae4f43
keybad <truekeybad@gmail.com>
2022-03-26 11:00:12 +0400

    Basic html template

--- main.go
+++ main.go
@@ -67,6 +67,66 @@ var (
 	file string
 )
 
+const (
+	pageBefore = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>YourLibrary to html converter</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/select/1.3.4/css/select.dataTables.min.css" rel="stylesheet"/>
+<link href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.bootstrap5.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.3.4/js/dataTables.select.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.bootstrap5.min.js"></script>
+<script>
+$(document).ready(function(){ 
+	$("table").DataTable({
+		paging: false,
+		select: true,
+		dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+	});
+});
+</script>
+</head>
+<body>
+<div class="container">
+`
+	pageAfter = `</div>
+</body>
+</html>
+`
+	tableBefore = `<div class="table-responsive">
+<table class="table" data-order='[[0,"asc"]]'>
+<thead>
+<tr>
+<th scope="col">#</th>
+<th scope="col">Artist</th>
+<th scope="col">Track</th>
+<th scope="col">Album</th>
+</tr>
+</thead>
+<tbody>
+`
+	tableAfter = `</tbody>
+</table>
+</div>
+`
+	tableTracksRow = `<tr><th scope="col">%d</th><td>%s</td><td>%s</td><td>%s</td></tr>
+`
+	titleRow = `<h2>%s</h2>
+`
+)
+
 func fileNameWithoutExt(fileName string) string {
 	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
 }
@@ -132,21 +192,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, _ = w.WriteString(`<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>YourLibrary to html converter</title>
-</head>
-<body>`)
+	_, _ = w.WriteString(pageBefore)
 
-	//TODO
+	if len(library.Tracks) > 0 {
+		_, _ = w.WriteString(fmt.Sprintf(titleRow, "Tracks"))
 
-	_, _ = w.WriteString(`
-</body>
-</html>
-`)
+		_, _ = w.WriteString(tableBefore)
+
+		for k, v := range library.Tracks {
+			_, _ = w.WriteString(fmt.Sprintf(tableTracksRow, k+1, v.Artist, v.Track, v.Album))
+		}
+
+		_, _ = w.WriteString(tableAfter)
+	}
+
+	_, _ = w.WriteString(pageAfter)
 
 	log.Println("Done!")
 }
